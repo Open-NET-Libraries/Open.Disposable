1b5c7311452228633ff61e97c19e939f201c465b
electricessence <5899455+electricessence@users.noreply.github.com>
2021-09-28 21:22:16 -0700

    Re-added suppressions.

--- DisposableBase.cs
+++ DisposableBase.cs
@@ -9,6 +9,10 @@ namespace Open.Disposable
 	public abstract class DisposableBase : DisposableStateBase, IDisposable
 	{
 		/// <inheritdoc />
+		[System.Diagnostics.CodeAnalysis.SuppressMessage(
+			"Usage",
+			"CA1816:Dispose methods should call SuppressFinalize",
+			Justification = "Finalization is too much of an edge case and can degrade performance.")]
 		public void Dispose()
 		{
 			if (!StartDispose())
